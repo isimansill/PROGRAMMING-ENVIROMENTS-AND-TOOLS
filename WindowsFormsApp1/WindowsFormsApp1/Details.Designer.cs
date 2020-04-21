b196a62ceaeabf585b09a284315208886809f147
Isidro Mansilla Pérez <isimansill@gmail.com>
2020-04-21 15:30:46 +0200

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Details.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Details
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cbPropertyType = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelArea = new System.Windows.Forms.Label();
+            this.textArea = new System.Windows.Forms.TextBox();
+            this.textAddress = new System.Windows.Forms.TextBox();
+            this.textYear = new System.Windows.Forms.TextBox();
+            this.textRooms = new System.Windows.Forms.TextBox();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textPrice = new System.Windows.Forms.TextBox();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnSelectPicture = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnAddPicture = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lblPicture = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(129, 86);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(142, 22);
+            this.txtName.TabIndex = 0;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(25, 86);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(45, 17);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Id";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(129, 41);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(142, 22);
+            this.txtId.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(309, 167);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // cbPropertyType
+            // 
+            this.cbPropertyType.FormattingEnabled = true;
+            this.cbPropertyType.Location = new System.Drawing.Point(446, 88);
+            this.cbPropertyType.Name = "cbPropertyType";
+            this.cbPropertyType.Size = new System.Drawing.Size(121, 24);
+            this.cbPropertyType.TabIndex = 5;
+            this.cbPropertyType.SelectedIndexChanged += new System.EventHandler(this.cbPropertyType_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(306, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Property Type";
+            // 
+            // labelArea
+            // 
+            this.labelArea.AutoSize = true;
+            this.labelArea.Location = new System.Drawing.Point(25, 173);
+            this.labelArea.Name = "labelArea";
+            this.labelArea.Size = new System.Drawing.Size(38, 17);
+            this.labelArea.TabIndex = 8;
+            this.labelArea.Text = "Area";
+            // 
+            // textArea
+            // 
+            this.textArea.Location = new System.Drawing.Point(129, 173);
+            this.textArea.Name = "textArea";
+            this.textArea.Size = new System.Drawing.Size(142, 22);
+            this.textArea.TabIndex = 9;
+            // 
+            // textAddress
+            // 
+            this.textAddress.Location = new System.Drawing.Point(129, 217);
+            this.textAddress.Name = "textAddress";
+            this.textAddress.Size = new System.Drawing.Size(142, 22);
+            this.textAddress.TabIndex = 10;
+            // 
+            // textYear
+            // 
+            this.textYear.Location = new System.Drawing.Point(129, 259);
+            this.textYear.Name = "textYear";
+            this.textYear.Size = new System.Drawing.Size(142, 22);
+            this.textYear.TabIndex = 11;
+            // 
+            // textRooms
+            // 
+            this.textRooms.Location = new System.Drawing.Point(446, 41);
+            this.textRooms.Name = "textRooms";
+            this.textRooms.Size = new System.Drawing.Size(121, 22);
+            this.textRooms.TabIndex = 12;
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(25, 217);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(60, 17);
+            this.labelAddress.TabIndex = 8;
+            this.labelAddress.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Year";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(306, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Rooms";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 127);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Price";
+            // 
+            // textPrice
+            // 
+            this.textPrice.Location = new System.Drawing.Point(129, 127);
+            this.textPrice.Name = "textPrice";
+            this.textPrice.Size = new System.Drawing.Size(142, 22);
+            this.textPrice.TabIndex = 18;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(610, 167);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 19;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(406, 167);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 20;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(508, 167);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 21;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnSelectPicture
+            // 
+            this.btnSelectPicture.Location = new System.Drawing.Point(397, 214);
+            this.btnSelectPicture.Name = "btnSelectPicture";
+            this.btnSelectPicture.Size = new System.Drawing.Size(155, 23);
+            this.btnSelectPicture.TabIndex = 22;
+            this.btnSelectPicture.Text = "Select Picture";
+            this.btnSelectPicture.UseVisualStyleBackColor = true;
+            this.btnSelectPicture.Click += new System.EventHandler(this.btnPicture_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnAddPicture
+            // 
+            this.btnAddPicture.Location = new System.Drawing.Point(584, 314);
+            this.btnAddPicture.Name = "btnAddPicture";
+            this.btnAddPicture.Size = new System.Drawing.Size(154, 23);
+            this.btnAddPicture.TabIndex = 24;
+            this.btnAddPicture.Text = "Add Picture";
+            this.btnAddPicture.UseVisualStyleBackColor = true;
+            this.btnAddPicture.Click += new System.EventHandler(this.btnAddPicture_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(584, 360);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(154, 23);
+            this.btnDelete.TabIndex = 25;
+            this.btnDelete.Text = "Delete Picture";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // lblPicture
+            // 
+            this.lblPicture.AutoSize = true;
+            this.lblPicture.Location = new System.Drawing.Point(397, 258);
+            this.lblPicture.Name = "lblPicture";
+            this.lblPicture.Size = new System.Drawing.Size(0, 17);
+            this.lblPicture.TabIndex = 26;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(397, 278);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(155, 137);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 27;
+            this.pictureBox.TabStop = false;
+            // 
+            // Details
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.lblPicture);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAddPicture);
+            this.Controls.Add(this.btnSelectPicture);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.textPrice);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textRooms);
+            this.Controls.Add(this.textYear);
+            this.Controls.Add(this.textAddress);
+            this.Controls.Add(this.textArea);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.labelArea);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbPropertyType);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.txtName);
+            this.Name = "Details";
+            this.Text = "Details";
+            this.Load += new System.EventHandler(this.Details_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ComboBox cbPropertyType;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelArea;
+        private System.Windows.Forms.TextBox textArea;
+        private System.Windows.Forms.TextBox textAddress;
+        private System.Windows.Forms.TextBox textYear;
+        private System.Windows.Forms.TextBox textRooms;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textPrice;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnSelectPicture;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnAddPicture;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label lblPicture;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Entities/Property.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Entities
+{
+    public enum PropertyTypes
+    {
+        Appartment = 1,
+        House = 2,
+        Ground = 3
+    }
+    public class Property
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Area { get; set; }
+        public string Address { get; set; }
+        public int Year { get; set; }
+        public int Rooms { get; set; }
+
+        public List<Picture> Pictures { get; set; }
+        public int PropertyType { get; set; }
+        public Property()
+        {
+            Pictures = new List<Picture>();
+        }
+    }
+}
